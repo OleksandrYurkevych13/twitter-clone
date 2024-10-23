d458432e859767663ca3ebc80f8909ee746469d1
Oleksandr Yurkevych <s.yurkevich13@gmail.com>
2024-10-23 20:54:28 +0200

    1- auth controller-added

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,19 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
-
-
-
-
+import cookieParser from "cookie-parser";
 
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 7000;
 
-console.log(process.env.MONGO_URI);
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
